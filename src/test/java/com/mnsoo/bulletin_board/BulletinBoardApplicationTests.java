9090ed8ec322513174fe947d36caf6623ebc992d
minsoo0506 <mspark010506@gmail.com>
2024-07-31 17:20:23 +0900

    chore: init

---
+++ src/test/java/com/mnsoo/bulletin_board/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnsoo.bulletin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
