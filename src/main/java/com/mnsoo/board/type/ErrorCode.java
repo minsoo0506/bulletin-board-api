467fd7528f55b1e650f968d0062feb1b74c32d87
minsoo0506 <mspark010506@gmail.com>
2024-08-01 17:39:16 +0900

    feat(exception): add exception handler

---
+++ src/main/java/com/mnsoo/board/type/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.mnsoo.board.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서부 오류가 발생했습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String description;
+}
